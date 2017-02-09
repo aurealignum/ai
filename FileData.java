236b1f5322842f119bba8954a894b93f307c24f2
aurealignum <aurealignum@gmail.com>
2017-02-08 20:11:37 -0600

    Update FileData.java

--- FileData.java
+++ FileData.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class FileData {
     public static void main (String[] args)throws IOException {
-        String file_name = "C:\\Users\\at018719\\Desktop\\test.txt";
+        String file_name = "C:\\Users\\computer\\Desktop\\test.txt";
         
         /*
         try{
@@ -23,30 +23,42 @@ public class FileData {
         //functional code here \/
         
         System.out.println("Hello");
-        sc.useDelimiter("\n"); //set scanner to accept " " as one token
+        sc.useDelimiter("\n"); //set scanner to split responses into lines
         String response = sc.next();
         
+        //check response against text file
         try{
             ReadFile file = new ReadFile(file_name);
             String[] aryLines = file.OpenFile();
+            int i = 0;
             
             //scan through all of the file
-            for (int i = 0; i < 10; i++){
-                
+            try {
+            	System.out.println(file.readLines());
+            	System.out.println(aryLines.length);
+            	while (aryLines.length <= file.readLines()){
+            		if (aryLines[i].equalsIgnoreCase(response)){
+            			System.out.println(aryLines[i+1]);
+            		}
+            		System.out.println(i + aryLines[i] + ":" + response);
+            		i = i+2;
+            		
+            	}
+            	
+            } catch (ArrayIndexOutOfBoundsException a) {
+            	//System.out.println(a.getMessage());
             }
         } catch (IOException e) {
             System.out.println( e.getMessage());
         }
         
-        if (response.equals("Hey")){
-            System.out.println("Hey fam");
-        } else {
-            System.out.println("Sorry I dont know what that means, what is the response?");
-            String proper_response = sc.next();
-            data.writeToFile(response + "_" + proper_response); 
-        }
+       
+        System.out.println("Sorry I dont know what that means, what is the response?");
+        String proper_response = sc.next();
+        data.writeToFile(response + proper_response); 
         
         
+        sc.close();
         System.out.println("File written to");
     }
 }
