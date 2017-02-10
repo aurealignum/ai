0adceec5003324a52bfeb62843e56f19a2acfdd4
aurealignum <aurealignum@gmail.com>
2017-02-10 07:31:53 -0600

    Update FileData.java

--- FileData.java
+++ FileData.java
@@ -1,5 +1,6 @@
 
 
+
 package textfile;
 import java.io.IOException;
 import java.util.Scanner;
@@ -33,40 +34,45 @@ public class FileData {
             String[] aryLines = file.OpenFile();
             int i = 0;
             int isSame = 0;
-            
+            boolean at = true;
             //scan through all of the file
             try {
                 int lines = file.readLines(); //changing lines to a local var so i can stop loop
-                
+                while (at = true) {
             	System.out.println(file.readLines());
             	System.out.println(aryLines.length);
-            	while (lines > aryLines.length){
-            		if (aryLines[i].equalsIgnoreCase("hi")){
+            	System.out.println(lines + ">" + aryLines.length);
+            		while (lines >= aryLines.length){
+            		
+            			if (aryLines[i].equalsIgnoreCase("hi")){
                             isSame = 1;
                             System.out.println("true");
-            		} else {
+            			} else {
                             isSame = 2;
+                            at = false;
                         }  
-                        //the while loop isnt getting here
+                        
                         if (isSame == 1) {
                             lines = lines * -1;
                             System.out.println(aryLines[i+1]);
-                            System.out.println("first path");
+                            response = sc.next();
+                            System.out.println(response);
                         } else {
                             i = i+2;
                         }
                         
                         //end of while
                     }
-                
-                    if (isSame == 2) {
-                        System.out.println("Sorry I dont know what that means, what is the response?");
-                        String proper_response = sc.next();
-                        data.writeToFile(response + proper_response); 
-                    }
+                }
+                System.out.println("Sorry I dont know what that means, what is the response?");
+                String proper_response = sc.next();
+                data.writeToFile(response + proper_response); 
                 
             } catch (ArrayIndexOutOfBoundsException a) {
-            	System.out.println("arrayindexoutofboundsexception");
+            	System.out.println("Sorry I dont know what that means, what is the response?");
+                String proper_response = sc.next();
+                data.writeToFile(response + proper_response); 
+            	//System.out.println("arrayindexoutofboundsexception");
             }
         } catch (IOException e) {
             System.out.println( e.getMessage());
